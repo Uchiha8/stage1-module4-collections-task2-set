a2ef8295ec1e75d1809d3e6507cf6896d3f601e2
[200~2110107@newuu.uz <2110107@newuu.uz>
2025-03-01 13:20:43 +0500

    Implement methods to create HashSet, Set combinations, and subsets of squares

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -2,8 +2,26 @@ package com.epam.mjc.collections.set;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+        HashSet<Integer> hashSet = new HashSet<>();
+        for (Integer num : sourceList) {
+            if (num % 2 == 0) {
+                while (num > 0) {
+                    hashSet.add(num);
+                    num /= 2;
+                }
+            } else {
+                hashSet.add(num);
+                if (num < 0) {
+                    hashSet.add(2 * num * (-1));
+                } else {
+                    hashSet.add(2 * num);
+                }
+            }
+        }
+        return hashSet;
     }
 }
--- src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
@@ -5,5 +5,17 @@ import java.util.Set;
 
 public class SetCombinationCreator {
     public Set<String> createSetCombination(Set<String> firstSet, Set<String> secondSet, Set<String> thirdSet) {
+        Set<String> stringSet = new HashSet<>();
+        for (String str : firstSet) {
+            if (secondSet.contains(str) && !thirdSet.contains(str)) {
+                stringSet.add(str);
+            }
+        }
+        for (String str : thirdSet) {
+            if (!firstSet.contains(str) && !secondSet.contains(str)) {
+                stringSet.add(str);
+            }
+        }
+        return stringSet;
     }
 }
